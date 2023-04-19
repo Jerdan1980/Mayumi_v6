7aaf2afe715977674fb4cd7f9d3e7827004b48e3
Jerdan1980 <jeremytinana@e2-micro-1.us-east1-b.c.cods-mayumi.internal>
2023-04-19 03:22:14 +0000

    Add await to prevent crashes

--- events/starboardAlreadyStarred.js
+++ events/starboardAlreadyStarred.js
@@ -2,6 +2,10 @@ module.exports = {
 	name: 'starboardAlreadyStarred',
 	starboard: true,
 	async execute(emoji, message, user) {
-		user.send(`This message is already in _${message.client.guilds.resolve(message.guildId).name}_'s starboard.`);
+		try {
+			await user.send(`This message is already in _${message.client.guilds.resolve(message.guildId).name}_'s starboard.`);
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
--- events/starboardNoSelfStar.js
+++ events/starboardNoSelfStar.js
@@ -2,6 +2,10 @@ module.exports = {
 	name: 'starboardNoSelfStar',
 	starboard: true,
 	async execute(emoji, message, user) {
-		user.send(`You cannot ${emoji} your own messages in _${message.client.guilds.resolve(message.guildId).name}_.`);
+		try {
+			await user.send(`You cannot ${emoji} your own messages in _${message.client.guilds.resolve(message.guildId).name}_.`);
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
